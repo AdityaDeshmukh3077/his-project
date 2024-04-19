27dff475812b15fc30030a3e6596e2b1e90ecbdd
AdityaDeshmukh3077 <adityadeshmukh3077@gmail.com>
2024-04-19 15:54:57 -0400

    Chore : Update

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request
+import util
+
+
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', methods=['GET'])
 @app.route('/login', methods=['POST'])
 def home_page():
     if request.method == 'POST':
-        print("Hello World!")
+        username = request.form['username']
+        password = request.form['password']
+        if util.check_username_password(username, password):
+            return render_template('forms1.html')
+        else:
+            print("Hello World")
     else:
         return render_template('index.html')
---
+++ util.py
@@ -0,0 +1,26 @@
+import psycopg2
+import pandas as pd
+
+def check_username_password(username, password):
+    try:
+        conn = psycopg2.connect(
+            dbname="his",
+            user="dadb",
+            host="127.0.0.1",
+            port="5432"
+        )
+
+        cur = conn.cursor()
+        cur.execute("SELECT PASSWORD FROM USERS WHERE USERID = %s", (username,))
+        row = cur.fetchone()  
+        if row:  
+            stored_password = row[0]
+            if stored_password == password:
+                return True
+        return False
+    except psycopg2.Error as e:
+        print("Database error:", e)
+        return False
+    finally:
+        if conn:
+            conn.close()
